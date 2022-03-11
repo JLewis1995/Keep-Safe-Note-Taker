96445935d684120f137ac75f53cd0512b4ecb3a0
Jacob Lewis <jacobllewis@outlook.com>
2022-03-11 11:34:42 -0700

    updated server to include routes

---
+++ routes/api.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const fs = require('fs');
+
+// router.get
+
+
+// router.post
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const path = require('path');
+const api = require('./routes/api');
+const html = require('./routes/html');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
-);
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+app.use('/api', api);
+app.use('/', html);
 
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT}`)
