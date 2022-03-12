74c537925fd92c44642c6635641ef2ab96c96fba
Jacob Lewis <jacobllewis@outlook.com>
2022-03-11 22:01:04 -0700

    delete testing

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -82,7 +82,7 @@ const handleNoteDelete = (e) => {
   e.stopPropagation();
 
   const note = e.target;
-  const noteId = e.parent().id;
+  const noteId = JSON.parse(note.parentElement).id;
 
   if (activeNote.id === noteId) {
     activeNote = {};
